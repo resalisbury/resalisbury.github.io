10f07c67941231a3b388ad9012345e80b4154e2a
Your Name <rex.salisbury@gmail.com>
2015-04-02 12:00:13 -0700

    test

--- js/location.js
+++ js/location.js
@@ -1,12 +1,11 @@
 console.log("hello")
 getLocation();
-google.maps.event.addDomListener(window, 'load', initialize);
+initialize();
 
 var map;
 var infowindow;
 var latitude;
 var longitude;
-var myLatLng;
 
 
 function getLocation() {
@@ -20,7 +19,7 @@ function getLocation() {
 function setLatLong(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  myLatLng = new google.maps.LatLng(latitude, longitude);
+  var myLatLng = new google.maps.LatLng(latitude, longitude);
   var myLocation = new google.maps.Marker({
     position: myLatLng,
     map: map,
@@ -28,15 +27,15 @@ function setLatLong(position) {
 }
 
 function initialize() {
-  // var pyrmont = new google.maps.LatLng(latitude, longitude);
+  var pyrmont = new google.maps.LatLng(37.7613981, -122.40302159999999);
 
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: myLatLng,
+    center: pyrmont,
     zoom: 12
   });
 
   var request = {
-    location: myLatLng,
+    location: pyrmont,
     radius: 8000,
     types: ['bar'],
     open_now: 'true',
