f79034c594d52ece6e97858c1a210e0039d92550
Your Name <rex.salisbury@gmail.com>
2015-04-02 12:18:14 -0700

    test

--- js/location.js
+++ js/location.js
@@ -7,7 +7,7 @@ var myLatLng;
 console.log("hello");
 getLocation();
 console.log(myLatLng);
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize(latitude, longitude));
 
 
 
@@ -30,16 +30,16 @@ function setLatLong(position) {
   });
 }
 
-function initialize() {
-  // var pyrmont = new google.maps.LatLng(37.7613981, -122.40302159999999);
+function initialize(lat, longit) {
+  var centerMap = new google.maps.LatLng(lat, longit);
 
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: myLatLng,
+    center: centerMap,
     zoom: 12
   });
 
   var request = {
-    location: myLatLng,
+    location: centerMap,
     radius: 8000,
     types: ['bar'],
     open_now: 'true',
