bdf8fc741939b49bc473bc9d25a3c20fa7051f73
Your Name <rex.salisbury@gmail.com>
2015-04-02 10:29:04 -0700

    update lat long

--- js/location.js
+++ js/location.js
@@ -25,15 +25,15 @@ function showPosition(position) {
 }
 
 function initialize() {
-  var pyrmont = new google.maps.LatLng(37.7613981, -122.40302159999999);
+  var currentLatLong = new google.maps.LatLng(latitude, longitude);
 
   map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: pyrmont,
+    center: currentLatLong,
     zoom: 15
   });
 
   var request = {
-    location: pyrmont,
+    location: currentLatLong,
     radius: 8000,
     types: ['bar'],
     open_now: 'true',
